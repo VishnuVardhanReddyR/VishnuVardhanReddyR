62ba0d9129c4a84b12531fcc160d65de712bcf74
R Vishnu Vardhan Reddy <36665976+VishnuVardhanReddyR@users.noreply.github.com>
2022-02-04 20:53:31 +0530

    class name correction

--- components/AboutComponents/MyOverview.js
+++ components/AboutComponents/MyOverview.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 function MyOverview() {
   return (
-    <section class="text-gray-400 body-font">
-      <div class="container px-5 py-24 mx-auto flex flex-wrap">
-          <h2 class="sm:text-3xl text-2xl text-gray-300 font-medium title-font mb-2 md:w-2/5">
+    <section className="text-gray-400 body-font">
+      <div className="container px-5 py-24 mx-auto flex flex-wrap">
+          <h2 className="sm:text-3xl text-2xl text-gray-300 font-medium title-font mb-2 md:w-2/5">
             As a Software Engineer and Web Developer, I am Vishnu Vardhan.
           </h2>
-        <div class="md:w-3/5 md:pl-6">
-          <p class="leading-relaxed text-base">
+        <div className="md:w-3/5 md:pl-6">
+          <p className="leading-relaxed text-base">
             As a freelancer based in India, I have been developing noteworthy websites for years that follow current trends. It is my job to turn ideas into solutions. Being able to see product evolution clearly helps me prioritize tasks, iterate promptly, and deliver projects faster.
           </p>
-          <div class="flex md:mt-4 mt-6">
+          <div className="flex md:mt-4 mt-6">
               <h4 className="text-gray-400 mr-8"><b className="text-gray-300">Name: </b> Vishnu Vardhan Reddy Rapuru</h4>
               <h4 className="text-gray-400"><b className="text-gray-300">Age: </b>23</h4>
           </div>
-          <div class="flex md:mt-4 mt-6">
+          <div className="flex md:mt-4 mt-6">
               <h4 className="text-Primary mr-10"><b className="text-gray-300 mr-2">Email: </b><a href="mailto:vishnurapuru@gmail.com">vishnurapuru10@gmail.com</a></h4>
               <h4 className="text-gray-400"><b className="text-gray-300">From: </b>Hyderabad, India</h4>
           </div>
--- components/AboutComponents/MyServices.js
+++ components/AboutComponents/MyServices.js
@@ -2,41 +2,41 @@ import React from 'react'
 
 function MyServices() {
   return (
-    <section class="text-gray-400 body-font">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-col">
-          <div class="h-1 bg-gray-900 rounded overflow-hidden">
-            <div class="w-48 h-full bg-Primary"></div>
+    <section className="text-gray-400 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col">
+          <div className="h-1 bg-gray-900 rounded overflow-hidden">
+            <div className="w-48 h-full bg-Primary"></div>
           </div>
-          <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-2">
-            <h1 class="sm:w-2/5 text-gray-300 font-medium title-font text-2xl sm:mb-0">My Services</h1>
+          <div className="flex flex-wrap sm:flex-row flex-col py-6 mb-2">
+            <h1 className="sm:w-2/5 text-gray-300 font-medium title-font text-2xl sm:mb-0">My Services</h1>
           </div>
         </div>
-        <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
-          <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-            <div class="rounded-lg h-32 overflow-hidden">
-              <img alt="content" class="object-cover object-center h-32 w-32" src="/WebDev.png" />
+        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div className="rounded-lg h-32 overflow-hidden">
+              <img alt="content" className="object-cover object-center h-32 w-32" src="/WebDev.png" />
             </div>
-            <h2 class="text-xl font-medium title-font text-gray-300 mt-5">Web Development</h2>
-            <p class="text-base leading-relaxed mt-2">
+            <h2 className="text-xl font-medium title-font text-gray-300 mt-5">Web Development</h2>
+            <p className="text-base leading-relaxed mt-2">
               Web Development using Frontend-frameworks such as React.js and Next.js
             </p>
           </div>
-          <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-            <div class="rounded-lg h-32 overflow-hidden">
-              <img alt="content" class="object-cover object-center h-32 w-32" src="/responsive.png" />
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div className="rounded-lg h-32 overflow-hidden">
+              <img alt="content" className="object-cover object-center h-32 w-32" src="/responsive.png" />
             </div>
-            <h2 class="text-xl font-medium title-font text-gray-300 mt-5">Fully Responsive</h2>
-            <p class="text-base leading-relaxed mt-2">
+            <h2 className="text-xl font-medium title-font text-gray-300 mt-5">Fully Responsive</h2>
+            <p className="text-base leading-relaxed mt-2">
               Websites that are Fully Responsive and compatible with all screen sizes 
             </p>
           </div>
-          <div class="p-4 md:w-1/3 sm:mb-0 mb-6">
-            <div class="rounded-lg h-32 overflow-hidden">
-              <img alt="content" class="object-cover object-center h-32 w-32" src="/photoshop.png" />
+          <div className="p-4 md:w-1/3 sm:mb-0 mb-6">
+            <div className="rounded-lg h-32 overflow-hidden">
+              <img alt="content" className="object-cover object-center h-32 w-32" src="/photoshop.png" />
             </div>
-            <h2 class="text-xl font-medium title-font text-gray-300 mt-5">PSD Design</h2>
-            <p class="text-base leading-relaxed mt-2">
+            <h2 className="text-xl font-medium title-font text-gray-300 mt-5">PSD Design</h2>
+            <p className="text-base leading-relaxed mt-2">
               Photshop skills for editing embedded images on a website
             </p>
           </div>
