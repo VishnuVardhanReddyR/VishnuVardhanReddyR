95db5cc5579233b1e2d53bb8af07ffdb48992f67
R Vishnu Vardhan Reddy <36665976+VishnuVardhanReddyR@users.noreply.github.com>
2022-02-09 17:24:23 +0530

    Update WorkIcon.js

--- components/subComponents/PortfolioComponents/WorkIcon.js
+++ components/subComponents/PortfolioComponents/WorkIcon.js
@@ -4,8 +4,8 @@ function WorkIcon({ inView, img, anime, border}) {
   return (
     <div 
         className={`${inView? `${anime}` : ``} 
-                sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full border-t-8 
-                ${border} border-gray-700 text-indigo-500 flex-shrink-0`}>
+                sm:w-32 sm:h-32 h-20 w-20 sm:mx-8 inline-flex items-center justify-center rounded-full border-t-8 
+                ${border} border-gray-700 flex-shrink-0`}>
             <img className="sm:w-16 sm:h-16 w-10 h-10" src={"/" + img} alt=""/>
     </div>
   )
